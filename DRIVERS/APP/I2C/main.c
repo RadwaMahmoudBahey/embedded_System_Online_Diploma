1e9d3e007dcef686a05ed1ecc07a4f0b4559aaa5
radwaa <radwaa@yahoo.com>
2023-08-28 08:17:24 +0300

    AVR I2C DRIVER

--- DRIVERS/APP/I2C/main.c
+++ DRIVERS/APP/I2C/main.c
@@ -1,62 +1,5 @@
 
 
-//
-//#define Act_As_Master
-////#define Act_As_Slave
-//
-//#define F_CPU 16000000UL
-//
-//#include "STD_TYPES.h"
-//#include "BIT_MATH.h"
-////2 HAL
-////3 MCAL
-//#include "DIO.h"
-//#include "I2C.h"
-////4
-//#include "avr/io.h"
-//#include "util/delay.h"
-//
-//
-//
-//int main()
-//{
-//#ifdef Act_As_Master
-//	unsigned char ch= 0;
-//	I2C_Init(0x48);
-//	while(1)
-//	{
-//		_delay_ms(500);
-//		I2C_Start();
-//		I2C_Write(0b11010000+0); // Transmit SLA+W(0)
-//		while((TWSR&0xF8)!=0x18);		// SLA+W has been transmitted ACK has been received
-//		I2C_Write(ch++);
-//		while((TWSR&0xF8)!=0x28);		// Data byte has been transmitted ACK has been received
-//		I2C_Stop();
-//	}
-//
-//
-//	return 0;
-//#endif
-//
-//#ifdef Act_As_Slave
-//	u8 status;
-//	DDRA = 0xFF;
-//	I2C_Set_Address(0b1101000);
-//	while(1){
-//		PORTA = I2C_Slave_Read(&status);
-//	}
-//
-//
-//	return 0;
-//#endif
-//
-//
-//}
-//
-//
-//
-
-
 
 
 /*
@@ -64,7 +7,7 @@
 
  *
  *  Created on: Aug 28, 2023
- *      Author: Dell
+ *      Author: Radwa
  */
 
 #define F_CPU 8000000ul
